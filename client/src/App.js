7e4fe5e45cc096b81eb209bd8d782eee53d42eb5
Kiran Bhatt <kb_43@outlook.com>
2019-05-30 22:33:50 +0545

    data received from socket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
+import socketOpen from "socket.io-client";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: "test"
+      messages: []
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const socket = socketOpen("http://localhost:3005");
+    socket.on("message", message => {
+      // console.log(message);
+      this.setState({
+        messages: [...this.state.messages, message]
+      });
+    });
+  }
   render() {
     return (
       <div>
-        <h1>{this.state.messages}</h1>
+        {this.state.messages &&
+          this.state.messages.map((data, index) => {
+            console.log(data);
+            return <li key={index}>{data.message}</li>;
+          })}
       </div>
     );
   }
