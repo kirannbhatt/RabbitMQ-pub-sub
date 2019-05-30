2b88a6277206db5f683921455efdfe3cefd41d49
Kiran Bhatt <kb_43@outlook.com>
2019-05-30 22:48:49 +0545

    message displayed in table

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import socketOpen from "socket.io-client";
+import { Table } from "semantic-ui-react";
 
 class App extends Component {
   constructor(props) {
@@ -21,11 +22,28 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.messages &&
-          this.state.messages.map((data, index) => {
-            console.log(data);
-            return <li key={index}>{data.message}</li>;
-          })}
+        <Table style={{ maxWidth: 650 }}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Message</Table.HeaderCell>
+              <Table.HeaderCell>Priority</Table.HeaderCell>
+              <Table.HeaderCell>Timestamp</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.state.messages &&
+              this.state.messages.map((data, index) => {
+                console.log(data);
+                return (
+                  <Table.Row key={index}>
+                    <Table.Cell>{data.message}</Table.Cell>
+                    <Table.Cell>{data.priority}</Table.Cell>
+                    <Table.Cell>{data.timestamp}</Table.Cell>
+                  </Table.Row>
+                );
+              })}
+          </Table.Body>
+        </Table>
       </div>
     );
   }
